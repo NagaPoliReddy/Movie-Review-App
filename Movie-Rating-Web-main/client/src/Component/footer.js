136ec54c06429866cac91e59956f7c7328833588
NagaPoliReddy <gudhibandinagapolireddy@gmail.com>
2022-10-12 23:15:57 +0530

    intial commit

---
+++ Movie-Rating-Web-main/client/src/Component/footer.js
@@ -0,0 +1,10 @@
+export default function footer() {
+    return (
+        <footer class="site-footer">
+            <div class="container">
+                <div class="colophon">Movie Review , Developed by G.Naga Poli Reddy</div>
+            </div>
+
+        </footer>
+    )
+}
